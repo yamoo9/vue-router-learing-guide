d84e0b9db345f096b7f7fc55454399f9421c9a75
yamoo9 <yamoo9@naver.com>
2024-06-07 20:41:18 +0900

    [구성] 리디렉션 & 에일리어스

---
+++ frontend/src/router.ts
@@ -0,0 +1,33 @@
+import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router'
+
+const routes: RouteRecordRaw[] = [
+  {
+    path: '/',
+    name: 'bottleList',
+    component: () => import('@/views/ProductListPage.vue')
+  },
+  {
+    path: '/bottle/:slug',
+    name: 'bottleItem',
+    component: () => import('@/views/ProductDetailPage.vue')
+  },
+  {
+    path: '/cart',
+    name: 'shoppingCart',
+    component: () => import('@/views/ShoppingCartPage.vue')
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    name: 'notFound',
+    component: () => import('@/views/NotFound.vue')
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
+  linkActiveClass: 'active-link',
+  linkExactActiveClass: 'exact-active-link'
+})
+
+export default router
