d39c7af1384fe65cf8282906e5f70d39d9d5f6f0
yamoo9 <yamoo9@naver.com>
2024-06-07 11:33:47 +0900

    [구성] 검색엔진 최적화(SEO) 설정

---
+++ frontend/src/router.ts
@@ -0,0 +1,28 @@
+import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router'
+
+const routes: RouteRecordRaw[] = [
+  {
+    path: '/',
+    name: 'bottleList',
+    component: () => import('@/views/ProductListPage.vue')
+  },
+  {
+    path: '/bottle/:productId',
+    name: 'bottleItem',
+    component: () => import('@/views/ProductDetailPage.vue')
+  },
+  {
+    path: '/cart',
+    name: 'shoppingCart',
+    component: () => import('@/views/ShoppingCartPage.vue')
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
+  linkActiveClass: 'active-link',
+  linkExactActiveClass: 'exact-active-link'
+})
+
+export default router
